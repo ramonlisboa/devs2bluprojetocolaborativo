08a9591d37c0515f53a742efefbf6d2785ac5575
Guilherme Fernando <guillhermefers@gmail.com>
2022-09-15 22:10:08 -0300

    [GuilhermeFernando] - NovoForm

--- SlnSharedProject/src/Devs2Blu.ProjetosAula.ViewProject/Program.cs
+++ SlnSharedProject/src/Devs2Blu.ProjetosAula.ViewProject/Program.cs
@@ -16,7 +16,7 @@ namespace Devs2Blu.ProjetosAula.ViewProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormGuilhermeFernando());
         }
     }
 }
