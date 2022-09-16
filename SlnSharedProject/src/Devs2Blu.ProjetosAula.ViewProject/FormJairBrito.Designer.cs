01d7f783f6940eea04c9b68430109ca772e7744c
Jailsonr12 <jailson.roth@outlook.comgit config --global user.email jailson.roth@outlook.com>
2022-09-16 18:43:04 -0300

    [Jailson Roth] - NovoFrom

--- SlnSharedProject/src/Devs2Blu.ProjetosAula.ViewProject/FormJairBrito.Designer.cs
+++ SlnSharedProject/src/Devs2Blu.ProjetosAula.ViewProject/FormJairBrito.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Devs2Blu.ProjetosAula.ViewProject
 {
-    partial class Form1
+    partial class FormJairBrito
     {
         /// <summary>
         /// Required designer variable.
@@ -31,10 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "FormJairBrito";
         }
 
         #endregion
     }
 }
-
