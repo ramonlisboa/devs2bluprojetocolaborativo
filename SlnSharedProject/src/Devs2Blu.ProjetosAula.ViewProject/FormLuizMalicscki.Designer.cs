5c2323350ae04e53309199178a534d2d37426b2d
Malicscki <luiz.malicscki@gmail.com>
2022-09-15 22:08:56 -0300

    [LuizMalicscki] - NovoForm

---
+++ SlnSharedProject/src/Devs2Blu.ProjetosAula.ViewProject/FormLuizMalicscki.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Devs2Blu.ProjetosAula.ViewProject
+{
+    partial class FormLuizMalicscki
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(208, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(491, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bem-vindo ao Projeto Malicscki";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FormLuizMalicscki
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "FormLuizMalicscki";
+            this.Text = "FormLuizMalicscki";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
