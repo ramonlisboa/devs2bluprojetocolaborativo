7dd7fc4ef9aef044614e5925767ddcc10e710968
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2022-09-15 22:10:06 -0300

    [RamonLisboa] - NovoForm

---
+++ SlnSharedProject/src/Devs2Blu.ProjetosAula.ViewProject/FormRamonLisboa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Devs2Blu.ProjetosAula.ViewProject
+{
+    public partial class FormRamonLisboa : Form
+    {
+        public FormRamonLisboa()
+        {
+            InitializeComponent();
+        }
+    }
+}
