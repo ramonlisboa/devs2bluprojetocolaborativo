d9ea7c23ad765844499f1140283b0b088a9fe7e9
Dimpeny <guilhermezimermann.grz@gmail.com>
2022-09-19 22:44:24 -0300

    [Guilherme Zimermann] - Atividade Aula POO 2

--- SlnSharedProject/src/Devs2Blu.ProjetosAula.ViewProject/FormGuilhermeRafaelZimermann.Designer.cs
+++ SlnSharedProject/src/Devs2Blu.ProjetosAula.ViewProject/FormGuilhermeRafaelZimermann.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(339, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Alo mundo!";
+            // 
+            // FormGuilhermeRafaelZimermann
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "FormGuilhermeRafaelZimermann";
             this.Text = "FormGuilhermeRafaelZimermann";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
